972efb89ea06d1e19d2d5590a7b8bf8e60a3dfcc
Ikumi Otani <matelediter@gmail.com>
2016-09-15 06:43:11 +0900

    run program

--- ANGLE.py
+++ ANGLE.py
@@ -18,6 +18,9 @@ import RTC
 import OpenRTM_aist
 
 
+#import to use ANGLE Unit
+import spidev
+
 # Import Service implementation class
 # <rtc-template block="service_impl">
 
@@ -49,6 +52,21 @@ angle_spec = ["implementation_id", "ANGLE",
 		 ""]
 # </rtc-template>
 
+###############
+#add my codes
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def readadc(channel):
+	adc = spi.xfer2([1,(8+channel)<<4,0])
+	data = ((adc[1]&3) << 8) + adc[2]
+	return data
+
+##############
+
+
+
 ##
 # @class ANGLE
 # @brief ModuleDescription
@@ -62,9 +80,13 @@ class ANGLE(OpenRTM_aist.DataFlowComponentBase):
 	# 
 	def __init__(self, manager):
 		OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
-
-		angle_arg = [None] * ((len(RTC._d_TimedShort) - 4) / 2)
-		self._d_angle = RTC.TimedShort(*angle_arg)
+		#Undercode Not available write()
+		#angle_arg = [None] * ((len(RTC._d_TimedShort) - 4) / 2)
+		#change code can write()
+		#self._d_angle = RTC.TimedShort(*angle_arg)
+		#Change code can use write()
+		self._d_angle = RTC.TimedShort(RTC.Time(0,0),0)
+		
 		"""
 		ANGLEユニットで取得したangle情報を整形してアウトする
 		 - Type: Short
@@ -85,6 +107,7 @@ class ANGLE(OpenRTM_aist.DataFlowComponentBase):
 		self._ANGLEPIN = [0]
 		
 		# </rtc-template>
+
 
 
 		 
@@ -192,7 +215,13 @@ class ANGLE(OpenRTM_aist.DataFlowComponentBase):
 		#
 		#
 	def onExecute(self, ec_id):
-	
+		#Debug ANGLESUB
+		ANGLESUB = 0
+		data = readadc(ANGLESUB)
+		print ("adc : {:8} ".format(data))
+		self._d_angle.data = data
+		self._AngleOut.write()
+		time.sleep(0.01)
 		return RTC.RTC_OK
 	
 	#	##
